b9433c06fdbd6f1286911d4b8625298894eb11c3
Jeremy Cantu <mail@jeremycantu.com>
2018-06-02 14:26:26 -0500

    austinchildguidance data scraping fin

--- austinchildguidance.go
+++ austinchildguidance.go
@@ -8,24 +8,40 @@ import (
 )
 
 func main() {
+	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+	// Find and grab all therapist photo links
+	c.OnHTML("div.collection-item__content", func(e *colly.HTMLElement) {
+		imgSrc := e.ChildAttr("img", "src")
+
+		// Print link
+		if imgSrc != "" {
+			fmt.Printf("Picture found: %s\n", imgSrc)
+		}
+	})
+
+	// Find and grab all available therapist information
+	c.OnHTML("div.collection-item-description", func(e *colly.HTMLElement) {
+		therapistInformation := e.ChildText("p")
+
+		// Print link
+		if therapistInformation != "" {
+			fmt.Printf("Information found: %s\n", therapistInformation)
+		}
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
 	})
 
-	c.Visit("https://www.austinchildguidance.org/")
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.Visit("https://www.austinchildguidance.org/about-us/board-staff.html")
 }
