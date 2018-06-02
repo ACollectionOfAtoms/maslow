69a57c61711571282c00095a2cc81aa419d3ab94
Ritika Mehta <mehtarit@umich.edu>
2018-06-02 14:35:42 -0500

    Modified GoodTherapy Scraper

--- goodTherapy.go
+++ goodTherapy.go
@@ -7,27 +7,16 @@ import (
 )
 
 func main() {
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("goodtherapy.org"),
-	)
+	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
+	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		e.Request.Visit(e.Attr("href"))
 	})
 
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL)
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://www.goodtherapy.org")
+	c.Visit("https://www.goodtherapy.org/therapists/tx/austin/")
 }
